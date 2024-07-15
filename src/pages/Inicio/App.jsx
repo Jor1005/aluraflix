c04cc4b9096fb2d40ef1f483b514b2e2a5e4ad1a
Jor1005 <abanto1005@gmail.com>
2024-07-15 06:58:43 -0500

    otro commit

--- src/pages/Inicio/App.jsx
+++ src/pages/Inicio/App.jsx
@@ -8,34 +8,25 @@ import videos from '../../data/db.json'
 
 // aca ira el formulario
 
-const categories = [
-
-  {
-    title:"FRONTEND",
-    color:"#6BD1FF"
-  },
-  {
-    title:"BACKEND",
-    color:"#00C86F"
-  },
-  {
-    title:"INOVACIÓN Y GESTIÓN",
-    color:"#FFBA05"
-  },
-]
-
-  const [videos, setvideos] = useState([])
-
-  useEffect(()=>{
-    fetch("https://my-json-server.typicode.com/Jor1005/aluraflix/videos")
-    .then(Response=> Response.json())
-    .then(data=>{
-    setvideos(data)
-  })
-  },[])
-  
-
 function App() {
+
+  const categories = [
+
+    {
+      title:"FRONTEND",
+      color:"#6BD1FF"
+    },
+    {
+      title:"BACKEND",
+      color:"#00C86F"
+    },
+    {
+      title:"INOVACIÓN Y GESTIÓN",
+      color:"#FFBA05"
+    },
+  ]
+
+ 
   return (
     <div>
       <GlobalStyles/>
@@ -48,6 +39,7 @@ function App() {
          key={category.title}/>
          )
        }
+
       </div>
     </div>
   );
